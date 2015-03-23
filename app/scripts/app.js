38195e55cd41c3bccf737cff057580ef9305dd93
Ben Miller <bmiller@longviewinc.com>
2015-03-23 13:46:38 +0100

    Deleting phonegap/www files (will exclude from repo). Adding camera phonegap plugin.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 (function (document) {
   'use strict';
 
+  console.log(navigator.camera.getPicture);
+
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
